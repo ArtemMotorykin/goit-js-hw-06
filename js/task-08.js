009d74202d64f071df985402022ada4b1075ceed
ArtemMotorykin <artem.ekipag@gmail.com>
2023-10-06 20:00:16 +0300

    task08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+document.querySelector('.login-form').addEventListener('submit', function (event) {
+event.preventDefault(); // Перед тим, як обробити відправку форми, вимикаємо перезавантаження сторінки
 
+const email = this.elements.email.value;
+const password = this.elements.password.value;
+
+if (!email || !password) {
+  alert('All fields must be filled in.');
+  } else {
+  const formData = {
+  email: email,
+  password: password,
+};
+
+console.log(formData);
+this.reset(); // Очищаємо значення полів форми
+}
+});
