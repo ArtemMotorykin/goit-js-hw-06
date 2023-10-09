58f23033df4753f445ecb114d440839291964491
ArtemMotorykin <artem.ekipag@gmail.com>
2023-10-09 21:51:26 +0300

    task001

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach(category => {
   const categoryName = category.firstElementChild.textContent;
-  const categoryElements = category.lastElementChild.children;
+  const categoryElements = category.lastElementChild.children.length;
 
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements}`);
